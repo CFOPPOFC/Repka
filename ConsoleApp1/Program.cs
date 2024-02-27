0e6690d1f821fc3a1303e39c83e22e2c457d4096
TRRT <LKJUKUTJUYTJUK@gmail.com>
2024-02-27 09:52:09 +0700

    bbb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,11 +7,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int b;
-            Console.WriteLine("Сколько раз написать репка?");
-            string a = Console.ReadLine();
-            int.TryParse(a, out b);
-            for(int i=0;i<b;i++)
-                Console.WriteLine("репка");
+            int B;
+            Console.WriteLine("Выбери режим\n1.выборочный\n2.бесконечный");
+            string A = Console.ReadLine();
+            int.TryParse(A, out B);
+            if(B==1)
+            {
+                Console.WriteLine("Сколько раз написать репка?");
+                string a = Console.ReadLine();
+                int.TryParse(a, out b);
+                for (int i = 0; i < b; i++)
+                    Console.WriteLine("репка");
+            }
+            if(B==2)
+            {
+                while(true)
+                {
+                    Console.WriteLine("Репка");
+                }
+            }
+            
         }
     }
         
